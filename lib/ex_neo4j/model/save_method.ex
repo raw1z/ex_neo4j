a91d7fb911d34cddfe4e096a079565ca30541a77
Rawane Zossou <dev@raw1z.fr>
2015-02-26 06:44:09 +0100

    create a save query generator
    responsible for creating a suitable neo4j query when saving a model

--- lib/ex_neo4j/model/save_method.ex
+++ lib/ex_neo4j/model/save_method.ex
@@ -1,9 +1,5 @@
 defmodule ExNeo4j.Model.SaveMethod do
   def generate(metadata) do
-    persisted_fields = metadata.fields
-                        |> Enum.filter(fn field -> field.transient == false end)
-                        |> Enum.map(fn field -> field.name end)
-
     quote do
       def save(%__MODULE__{validated: false, errors: nil}=model) do
         model = validate(model)
@@ -23,7 +19,7 @@ defmodule ExNeo4j.Model.SaveMethod do
 
       defp do_save(%__MODULE__{}=model) do
         is_new_record = ( model.id == nil )
-        {query, query_params} = if is_new_record, do: query_for_new_model(model), else: query_for_existing_model(model)
+        {query, query_params} = ExNeo4j.Model.SaveQueryGenerator.query_for_model(model, __MODULE__)
 
         case ExNeo4j.Db.cypher(query, query_params) do
           {:ok, []} ->
@@ -44,53 +40,6 @@ defmodule ExNeo4j.Model.SaveMethod do
           {:error, resp} ->
             {:nok, resp, model}
         end
-      end
-
-      defp current_datetime do
-        Chronos.Formatter.strftime(Chronos.now, "%Y-%0m-%0d %H:%M:%S +0000")
-      end
-
-      defp query_for_new_model(model) do
-        query = """
-        CREATE (n:#{@label} { properties })
-        RETURN id(n), n
-        """
-
-        properties = unquote(persisted_fields)
-                      |> Enum.map(fn name -> {name, Map.get(model, name)} end)
-                      |> Enum.filter(fn {_k,v} -> v != nil end)
-                      |> Enum.into Map.new
-
-        date = current_datetime
-        properties = properties
-          |> Map.put(:updated_at, date)
-          |> Map.put(:created_at, date)
-
-        query_params = %{
-          properties: properties
-        }
-
-        {query, query_params}
-      end
-
-      defp query_for_existing_model(model) do
-        properties = unquote(persisted_fields)
-                      |> Enum.filter(&(&1 != :id))
-                      |> Enum.map(fn name -> {name, Map.get(model, name)} end)
-                      |> Enum.filter(fn {_k,v} -> v != nil end)
-                      |> Enum.into Map.new
-
-        properties = properties
-                      |> Map.put("updated_at", current_datetime)
-                      |> Enum.filter(fn {_k,v} -> v != nil end)
-                      |> Enum.map(fn {k,v} -> "n.#{k} = #{inspect v}" end)
-
-        query = """
-        START n=node(#{model.id})
-        SET #{Enum.join(properties, ", ")}
-        """
-
-        {query, %{}}
       end
     end
   end
---
+++ lib/ex_neo4j/model/save_query_generator.ex
@@ -0,0 +1,127 @@
+defmodule ExNeo4j.Model.SaveQueryGenerator do
+  def query_for_model(model, module) do
+    if model.id == nil do
+      query_for_new_model(model, module)
+    else
+      query_for_existing_model(model, module)
+    end
+  end
+
+  defp query_for_new_model(model, module) do
+    query = query_template(model, module)
+
+    date = current_datetime
+    properties = model_properties(model, module)
+                  |> Map.put(:updated_at, date)
+                  |> Map.put(:created_at, date)
+
+    query_params = %{
+      properties: properties
+    }
+
+    {query, query_params}
+  end
+
+  defp query_for_existing_model(model, module) do
+    properties = model_properties(model, module)
+                  |> Map.put("updated_at", current_datetime)
+                  |> Enum.map(fn {k,v} -> "n.#{k} = #{inspect v}" end)
+
+    query = """
+    #{query_template(model, module)}SET #{Enum.join(properties, ", ")}
+    """
+
+    {query, %{}}
+  end
+
+  defp model_properties(model, module) do
+    module.metadata.fields
+      |> Enum.filter(&(&1.transient == false && &1.relationship == false))
+      |> Enum.map(&(&1.name))
+      |> Enum.map(&({&1, Map.get(model, &1)}))
+      |> Enum.filter(fn {k,v} -> k != :id && v != nil end)
+      |> Enum.into Map.new
+  end
+
+  defp current_datetime do
+    Chronos.Formatter.strftime(Chronos.now, "%Y-%0m-%0d %H:%M:%S +0000")
+  end
+
+  def get_field_value(model, field) do
+    value = Map.get(model, field.name) || []
+    List.flatten([value])
+  end
+
+  defp model_relationships(model, module) do
+    for relationship <- module.metadata.relationships do
+      module.metadata.fields
+        |> Enum.filter(&(&1.relationship == true))
+        |> Enum.filter(&(&1.name == normalize(relationship.name)))
+        |> Enum.map(&({relationship, get_field_value(model, &1)}))
+        |> Enum.filter(fn {_relationship, field_value} -> Enum.empty?(field_value) == false end)
+    end
+    |> List.flatten
+  end
+
+  defp query_template(model, module) do
+    if model.id != nil do
+      """
+      START n=node(#{model.id})
+      """
+    else
+      relationships = model_relationships(model, module)
+      if Enum.count(relationships) == 0 do
+        query_template_for_new_model_without_relationships(model, module)
+      else
+        query_template_for_new_model_with_relationships(model, module, relationships)
+      end
+    end
+  end
+
+  defp query_template_for_new_model_without_relationships(_model, module) do
+    """
+    CREATE (n:#{module.label} { properties })
+    RETURN id(n), n
+    """
+  end
+
+  defp clauses_for_relationship({relationship, field_value}) do
+    start_clauses = Enum.map(field_value, &("#{normalize(relationship.name)}_#{&1}=node(#{&1})"))
+    relationship_clauses = Enum.map(field_value, &("CREATE (n)-[:#{relationship.name}]->(#{normalize(relationship.name)}_#{&1})" ))
+    return_clauses = Enum.map(field_value, &(["id(#{normalize(relationship.name)}_#{&1})", "#{normalize(relationship.name)}_#{&1}"]))
+                     |> List.flatten
+
+    {start_clauses, relationship_clauses, return_clauses}
+  end
+
+  defp query_template_for_new_model_with_relationships(model, module, relationships) do
+    clauses = relationships
+              |> Enum.map(&(clauses_for_relationship(&1)))
+
+    start_clauses = clauses
+                    |> Enum.map(fn {start, _relationship, _return} -> start end)
+                    |> List.flatten
+                    |> Enum.join(", ")
+
+    relationship_clauses = clauses
+                            |> Enum.map(fn {_start, relationship, _return} -> relationship end)
+                            |> List.flatten
+                            |> Enum.join("\n")
+
+    return_clauses = clauses
+                      |> Enum.map(fn {_start, _relationship, return} -> return end)
+                      |> List.flatten
+                      |> Enum.join(", ")
+
+    """
+    START #{start_clauses}
+    CREATE (n:#{module.label} { properties })
+    #{relationship_clauses}
+    RETURN id(n), n, #{return_clauses}
+    """
+  end
+
+  defp normalize(relationship_name) do
+    relationship_name |> Atom.to_string |> String.downcase |> String.to_atom
+  end
+end
