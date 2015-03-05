75063bb5e644ee5feeacd328a940d61f19dc33e5
Rawane Zossou <dev@raw1z.fr>
2015-03-05 19:27:40 +0100

    add support for querying relationships
    by related node id only for now

--- lib/ex_neo4j/model/find_query_generator.ex
+++ lib/ex_neo4j/model/find_query_generator.ex
@@ -7,7 +7,7 @@ defmodule ExNeo4j.Model.FindQueryGenerator do
       query_with_id_and_relationships(id, relationships)
     end
   end
-    
+
   def query_with_properties(module, properties) do
     relationships = model_relationships(module)
     if Enum.empty?(relationships) do
@@ -33,6 +33,55 @@ defmodule ExNeo4j.Model.FindQueryGenerator do
   end
 
   def query_with_properties_and_relationships(module, properties, relationships) do
+    if have_required_relationships?(properties, relationships) do
+      query_with_properties_and_required_relationships(module, properties, relationships)
+    else
+      query_with_properties_and_optional_relationships(module, properties, relationships)
+    end
+  end
+
+  def query_with_properties_and_required_relationships(module, properties, relationships) do
+    is_queried = fn(relationship) ->
+      Enum.any?(properties, fn {key,_} -> key == normalize(relationship.name) end)
+    end
+
+    start_clauses = relationships
+                    |> Enum.map(&normalize(&1.name))
+                    |> Enum.map(&({&1, properties[&1]}))
+                    |> Enum.filter(fn {_, id} -> id != nil end)
+                    |> Enum.map(fn {name, id} -> "START #{name}=node(#{id})" end)
+                    |> Enum.join("\n")
+
+    required_match_clauses = relationships
+                              |> Enum.filter(&(is_queried.(&1)))
+                              |> Enum.map(&("MATCH (n)-[:#{&1.name}]->(#{normalize(&1.name)})"))
+                              |> Enum.join("\n")
+
+    optional_match_clauses = relationships
+                              |> Enum.filter(&(is_queried.(&1) == false))
+                              |> Enum.map(&("OPTIONAL MATCH (n)-[:#{&1.name}]->(#{normalize(&1.name)})"))
+                              |> Enum.join("\n")
+
+    return_clauses = relationships
+                      |> Enum.map(&(["id(#{normalize(&1.name)})", normalize(&1.name)]))
+                      |> List.flatten
+                      |> Enum.join(", ")
+
+    query_params = properties
+                    |> Enum.filter(fn {k,_} -> Enum.any?(relationships, &(normalize(&1.name) == k)) == false end)
+                    |> Enum.map(fn {k,v} -> "#{k}: #{inspect(v)}" end)
+                    |> Enum.join(", ")
+
+    """
+    #{start_clauses}
+    MATCH (n:#{module.label} {#{query_params}})
+    #{required_match_clauses}
+    #{optional_match_clauses}
+    RETURN id(n), n, #{return_clauses}
+    """
+  end
+
+  def query_with_properties_and_optional_relationships(module, properties, relationships) do
     match_clauses = relationships
                     |> Enum.map(&("OPTIONAL MATCH (n)-[:#{&1.name}]->(#{normalize(&1.name)})"))
                     |> Enum.join("\n")
@@ -75,10 +124,14 @@ defmodule ExNeo4j.Model.FindQueryGenerator do
     #{match_clauses}
     RETURN id(n), n, #{return_clauses}
     """
-    
   end
 
   defp normalize(relationship_name) do
     relationship_name |> Atom.to_string |> String.downcase |> String.to_atom
+  end
+
+  defp have_required_relationships?(properties, relationships) do
+    relationship_names = Enum.map(relationships, &(normalize(&1.name)))
+    Enum.any? properties, fn {key, _} -> Enum.find(relationship_names, &(&1 == key)) end
   end
 end
--- test/model/find_query_generator_test.exs
+++ test/model/find_query_generator_test.exs
@@ -25,6 +25,17 @@ defmodule Model.FindQueryGeneratorTest do
     """
   end
 
+  test "finds by properties and relationships" do
+    query = FindQueryGenerator.query_with_properties Person, age: 30, friend_of: 1
+    assert query == """
+    START friend_of=node(1)
+    MATCH (n:Test:Person {age: 30})
+    MATCH (n)-[:FRIEND_OF]->(friend_of)
+    OPTIONAL MATCH (n)-[:MARRIED_TO]->(married_to)
+    RETURN id(n), n, id(friend_of), friend_of, id(married_to), married_to
+    """
+  end
+
   test "find by id for model without relationships" do
     query = FindQueryGenerator.query_with_id ModelWithoutRelationShip, 1
     assert query == """
