5b4ed69cca11c8810e6cf4cf1b29fe3a401b34df
Rawane Zossou <dev@raw1z.fr>
2015-03-13 07:36:45 +0100

    change the serialization strategy
    define a serialization strategy which is more compatible with emberjs default rest adapter

--- lib/ex_neo4j/model/serializer.ex
+++ lib/ex_neo4j/model/serializer.ex
@@ -1,11 +1,14 @@
 defmodule ExNeo4j.Model.Serializer do
+  alias ExNeo4j.SerializationBuffer
+
   def serialize(module, models) when is_list(models) do
-    serialized_models = models |> Enum.map(&(serialize_attributes(module, &1)))
-    Map.put(%{}, Inflex.pluralize(resource_name(module)), serialized_models)
+    Enum.reduce models, %{}, fn (model, acc) ->
+      SerializationBuffer.add_model(acc, module, model)
+    end
   end
 
   def serialize(module, model) do
-    Map.put(%{}, resource_name(module), serialize_attributes(module, model))
+    SerializationBuffer.add_model(%{}, module, model)
   end
 
   def to_json(module, models) when is_list(models) do
@@ -16,65 +19,7 @@ defmodule ExNeo4j.Model.Serializer do
     serialize(module, model) |> transform_to_json
   end
 
-  def serialize_attributes(module, model) do
-    serialized_fields = serialized_fields(module)
-    relationship_fields = relationship_fields(module)
-    keys = Map.keys(model)
-
-    Enum.reduce keys, %{}, fn (key, acc) ->
-      attribute_label = attribute_label(key)
-
-      if Enum.find(serialized_fields, &(&1.name == key)) do
-        Map.put acc, attribute_label, attribute_value(model, key)
-      else
-        if field = Enum.find(relationship_fields, &(&1.name == key)) do
-          relationship = Enum.find(module.metadata.relationships, &(field_name_for_relationship(&1) == field.name))
-          related_instances = Map.get(model, key) || []
-          Map.put acc, attribute_label, Enum.map(related_instances, &__MODULE__.serialize_attributes(relationship.related_model, &1))
-        else
-          acc
-        end
-      end
-    end
-  end
-
-  defp attribute_label(key) do
-    key |> Atom.to_string |> Inflex.camelize(:lower)
-  end
-
-  defp attribute_value(model, key) do
-    value = Map.get(model, key)
-    if key == :errors and value != nil do
-      format_error_key = fn k -> k |> Inflex.camelize(:lower) |> String.to_atom end
-      value = value |> Enum.map(fn {k,v} -> {format_error_key.(k), v} end) |> Enum.into(%{})
-    end
-    value
-  end
-
-  defp resource_name(module) do
-    "#{module}"
-    |> String.split(".")
-    |> List.last
-    |> String.downcase
-    |> Inflex.singularize
-  end
-
   defp transform_to_json(value, options \\ []) do
     Poison.Encoder.encode(value, options) |> IO.iodata_to_binary
-  end
-
-  defp serialized_fields(module) do
-    module.metadata.fields
-    |> Enum.filter(&(&1.name == :id or &1.name == :errors or (&1.transient == false and &1.private == false)))
-    |> Enum.filter(&(&1.relationship == false))
-  end
-
-  defp relationship_fields(module) do
-    module.metadata.fields
-    |> Enum.filter(&(&1.relationship == true))
-  end
-
-  defp field_name_for_relationship(relationship) do
-    relationship.name |> Atom.to_string |> String.downcase |> String.to_atom
   end
 end
--- test/model/serialization_test.exs
+++ test/model/serialization_test.exs
@@ -3,17 +3,17 @@ defmodule Model.SerializationTest do
 
   test "serializes a model" do
     person = Person.build(name: "John DOE", email: "john@doe.fr", age: 30)
-    assert Person.to_json(person) == "{\"person\":{\"updatedAt\":null,\"name\":\"John DOE\",\"marriedTo\":[],\"id\":null,\"friendOf\":[],\"errors\":null,\"email\":\"john@doe.fr\",\"createdAt\":null,\"age\":30}}"
+    assert Person.to_json(person) == "{\"people\":[{\"updatedAt\":null,\"name\":\"John DOE\",\"marriedTo\":[],\"id\":null,\"friendOf\":[],\"errors\":null,\"email\":\"john@doe.fr\",\"createdAt\":null,\"age\":30}]}"
   end
 
   test "serializes an array of model" do
-    people = [Person.build(name: "John DOE"), Person.build(name: "Jane DOE")]
-    assert Person.to_json(people) == "{\"people\":[{\"updatedAt\":null,\"name\":\"John DOE\",\"marriedTo\":[],\"id\":null,\"friendOf\":[],\"errors\":null,\"email\":null,\"createdAt\":null,\"age\":null},{\"updatedAt\":null,\"name\":\"Jane DOE\",\"marriedTo\":[],\"id\":null,\"friendOf\":[],\"errors\":null,\"email\":null,\"createdAt\":null,\"age\":null}]}"
+    people = [Person.build(id: 1, name: "John DOE"), Person.build(id: 2, name: "Jane DOE")]
+    assert Person.to_json(people) == "{\"people\":[{\"updatedAt\":null,\"name\":\"John DOE\",\"marriedTo\":[],\"id\":1,\"friendOf\":[],\"errors\":null,\"email\":null,\"createdAt\":null,\"age\":null},{\"updatedAt\":null,\"name\":\"Jane DOE\",\"marriedTo\":[],\"id\":2,\"friendOf\":[],\"errors\":null,\"email\":null,\"createdAt\":null,\"age\":null}]}"
   end
 
   test "serializes a model with relationships" do
     john = Person.build(id: 1, name: "John DOE", email: "john@doe.fr", age: 30)
     jane = Person.build(name: "Jane DOE", email: "jane@doe.fr", age: 20, friend_of: [john])
-    assert Person.to_json(jane) == "{\"person\":{\"updatedAt\":null,\"name\":\"Jane DOE\",\"marriedTo\":[],\"id\":null,\"friendOf\":[{\"updatedAt\":null,\"name\":\"John DOE\",\"marriedTo\":[],\"id\":1,\"friendOf\":[],\"errors\":null,\"email\":\"john@doe.fr\",\"createdAt\":null,\"age\":30}],\"errors\":null,\"email\":\"jane@doe.fr\",\"createdAt\":null,\"age\":20}}"
+    assert Person.to_json(jane) == "{\"people\":[{\"updatedAt\":null,\"name\":\"Jane DOE\",\"marriedTo\":[],\"id\":null,\"friendOf\":[1],\"errors\":null,\"email\":\"jane@doe.fr\",\"createdAt\":null,\"age\":20},{\"updatedAt\":null,\"name\":\"John DOE\",\"marriedTo\":[],\"id\":1,\"friendOf\":[],\"errors\":null,\"email\":\"john@doe.fr\",\"createdAt\":null,\"age\":30}]}"
   end
 end
